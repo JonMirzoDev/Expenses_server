099128ba8aaed6aa8b37c34174721729f986692a
JonMirzoDev <islomjon.swe@gmail.com>
2024-08-18 17:13:48 +0500

    refresh token logic updated

--- auth.py
+++ auth.py
@@ -1,18 +1,13 @@
 from flask import Blueprint, jsonify, make_response, request
-from flask_jwt_extended import (create_access_token, get_jwt_identity,
-                                jwt_required, set_access_cookies)
+from flask_jwt_extended import (create_access_token, create_refresh_token,
+                                get_jwt_identity, jwt_required,
+                                set_access_cookies, set_refresh_cookies)
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from database import get_db
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/refresh', methods=['POST'])
-@jwt_required(refresh=True)
-def refresh_token():
-    identity = get_jwt_identity()
-    access_token = create_access_token(identity=identity)
-    return jsonify({'access_token': access_token})
 
 @auth.route('/register', methods=['POST'])
 def register():
@@ -35,15 +30,28 @@ def login():
     
     if user and check_password_hash(user[3], data['password']):
         access_token = create_access_token(identity={'username': user[1], 'email': user[2]})
+        refresh_token = create_refresh_token(identity={'username': user[1], 'email': user[2]})
+        
         response = make_response(jsonify({'message': 'Login successful', 'user': {'username': user[1], 'email': user[2]}}))
         set_access_cookies(response, access_token)
+        set_refresh_cookies(response, refresh_token)
+        
         response.set_cookie('access_token', access_token, httponly=True, secure=False, samesite='Lax')
+        response.set_cookie('refresh_token', refresh_token, httponly=True, secure=False, samesite='Lax')
+        
         return response
     return jsonify({'message': 'Invalid credentials'}), 401
 
+@auth.route('/refresh', methods=['POST'])
+@jwt_required(refresh=True)
+def refresh_token():
+    identity = get_jwt_identity()
+    new_access_token = create_access_token(identity=identity)
+    response = make_response(jsonify({'access_token': new_access_token}))
+    set_access_cookies(response, new_access_token)
+    return response
 
 @auth.route('/logout', methods=['POST'])
-@jwt_required()
 def logout():
     response = make_response(jsonify({'message': 'Logout successful'}))
     response.set_cookie('access_token', '', expires=0, httponly=True, secure=False, samesite='Lax')
