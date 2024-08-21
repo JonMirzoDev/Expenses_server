993add940e95275274f21c613f2f78757fa15fa6
JonMirzoDev <islomjon.swe@gmail.com>
2024-08-21 10:36:14 +0500

    preps for deployment

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import logging
+import os
 from datetime import timedelta
 
+from dotenv import load_dotenv
 from flask import Flask, request
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
@@ -11,9 +13,12 @@ from dashboard import dashboard as dashboard_blueprint
 from database import close_connection, get_db
 from expenses import expenses as expenses_blueprint
 
+load_dotenv()
+
 app = Flask(__name__)
 CORS(app, supports_credentials=True, origins=['http://localhost:3000'])
-app.config['JWT_SECRET_KEY'] = 'your_secret_key'
+
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=24)
 jwt = JWTManager(app)
@@ -37,4 +42,6 @@ def home():
     return "Welcome to the Expense Tracker!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    app.run(debug=False)
