006d0d9816030393e0a482379f7c2515e7ed2ce1
JonMirzoDev <islomjon.swe@gmail.com>
2024-08-21 19:32:41 +0500

    db updated

--- database.py
+++ database.py
@@ -7,8 +7,7 @@ DATABASE = 'expenses.db'
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        # Connect to the database using the URL
-        db = g._database = sqlite3.connect(DATABASE.split(':///')[-1])
+        db = g._database = sqlite3.connect(DATABASE)
     return db
 
 def close_connection(exception):
